dcba5a206e40fe59fe7fb982e521369d8f056f5a
David Hill <DavidPH55@verizon.net>
2015-03-22 20:05:18 -0400

    DUD: Split remaining weapon handling code from dud_abil.

--- src/DUDamned/code/dud_abil.c
+++ src/DUDamned/code/dud_abil.c
@@ -6,7 +6,7 @@
 //
 //-----------------------------------------------------------------------------
 //
-// Ability functions.
+// Ability handling.
 //
 //-----------------------------------------------------------------------------
 
@@ -93,7 +93,7 @@
 
 
 //----------------------------------------------------------------------------|
-// Static Variables                                                           |
+// Static Objects                                                             |
 //
 
 [[no_init]]
@@ -110,7 +110,7 @@ static Menu DUD_Menu[MAX_PLAYERS];
 
 
 //----------------------------------------------------------------------------|
-// Global Variables                                                           |
+// Extern Objects                                                             |
 //
 
 //
@@ -317,234 +317,6 @@ dud_ability_t DUD_Ability[3][ABILMAX] =
 
 dud_ability_t *DUD_AbilitySelected[MAX_PLAYERS][3];
 
-//
-// DUD_Ammo
-//
-dud_ammo_t DUD_Ammo[AMMOMAX] =
-{
-   // AMMO_CLIP
-   {
-      AMMO_CLIP,
-      180,
-      350,
-      s"Clip",
-      s"Clip",
-   },
-
-   // AMMO_SHEL
-   {
-      AMMO_SHEL,
-      60,
-      1050,
-      s"Shell",
-      s"Shell",
-   },
-
-   // AMMO_RCKT
-   {
-      AMMO_RCKT,
-      20,
-      700,
-      s"Rocket",
-      s"RocketAmmo",
-   },
-
-   // AMMO_CELL
-   {
-      AMMO_CELL,
-      240,
-      350,
-      s"Cell",
-      s"Cell",
-   },
-};
-
-//
-// DUD_Weapon
-//
-dud_weapon_t DUD_Weapon[7][WEAPMAX] =
-{
-   // WEAP1
-   {
-      // WEAP1_FIST
-      {
-         true,
-         AMMOMAX,
-         s"Lets you punch zombies with your bare hands like real\n"
-         s"men do. Heck yeah!",
-         NULL,
-         NULL,
-         s"Fist",
-         s"DUD_Fist",
-         s"FIST",
-      },
-
-      // WEAP1_CSAW
-      {
-         true,
-         AMMOMAX,
-         s"Chops up demons real quick-like.",
-         NULL,
-         NULL,
-         s"Chainsaw",
-         s"DUD_Chainsaw",
-         s"C. SAW",
-      },
-
-      // WEAP1_CFIST
-      {
-         true,
-         AMMOMAX,
-         s"Smash and chop with the finest of them.",
-         NULL,
-         NULL,
-         s"Chainfist",
-         s"DUD_Chainfist",
-         s"C. FIST",
-      },
-   },
-
-   // WEAP2
-   {
-      // WEAP2_PIST
-      {
-         true,
-         AMMO_CLIP,
-         s"Shoots bullets.",
-         NULL,
-         NULL,
-         s"Pistol",
-         s"DUD_Pistol",
-         s"PISTOL",
-      },
-   },
-
-   // WEAP3
-   {
-      // WEAP3_SHTG
-      {
-         true,
-         AMMO_SHEL,
-         s"Shoots shells.",
-         NULL,
-         NULL,
-         s"Shotgun",
-         s"DUD_Shotgun",
-         s"SHTGUN",
-      },
-
-      // WEAP3_SHT2
-      {
-         true,
-         AMMO_SHEL,
-         s"Shoots two shells. Slower than the shotgun, but more\npowerful.",
-         NULL,
-         NULL,
-         s"Super Shotgun",
-         s"DUD_SuperShotgun",
-         s"SHT2",
-      },
-   },
-
-   // WEAP4
-   {
-      // WEAP4_CGUN
-      {
-         true,
-         AMMO_CLIP,
-         s"Shoots bullets fast.",
-         NULL,
-         NULL,
-         s"Chaingun",
-         s"DUD_Chaingun",
-         s"C. GUN",
-      },
-
-      // WEAP4_DGUN
-      {
-         true,
-         AMMO_CLIP,
-         s"Shoots dakka.",
-         NULL,
-         NULL,
-         s"Dakkagun",
-         s"DUD_Dakkagun",
-         s"DAKKA",
-      },
-   },
-
-   // WEAP5
-   {
-      // WEAP5_RCKT
-      {
-         true,
-         AMMO_RCKT,
-         s"Shoots rockets.",
-         NULL,
-         NULL,
-         s"Rocket Launcher",
-         s"DUD_RocketLauncher",
-         s"ROCKET",
-      },
-   },
-
-   // WEAP6
-   {
-      // WEAP6_PLAS
-      {
-         true,
-         AMMO_CELL,
-         s"Shoots plasma fast.",
-         NULL,
-         NULL,
-         s"Plasma Rifle",
-         s"DUD_PlasmaRifle",
-         s"PLASMA",
-      },
-
-      // WEAP6_LASG
-      {
-         true,
-         AMMO_CELL,
-         s"Shoots plasma hitscans. No affiliation with 40K. Nope.",
-         NULL,
-         NULL,
-         s"Lasgun",
-         s"DUD_Lasgun",
-         s"LASGUN",
-      },
-   },
-
-   // WEAP7
-   {
-      // WEAP7_BFG9
-      {
-         true,
-         AMMO_CELL,
-         s"Shoots plasma.",
-         NULL,
-         NULL,
-         s"B.F.G. 9000",
-         s"DUD_BFG9000",
-         s"B.F.G.",
-      },
-
-      // WEAP7_RAIL
-      {
-         true,
-         AMMO_CELL,
-         s"Shoots piercing plasma.",
-         NULL,
-         NULL,
-         s"Rail Rifle",
-         s"DUD_RailRifle",
-         s"RAIL",
-      },
-   },
-};
-
-dud_weapon_t *DUD_WeaponSelected[MAX_PLAYERS][7];
-
 
 //----------------------------------------------------------------------------|
 // Static Functions                                                           |
@@ -707,16 +479,8 @@ static void DUD_MenuInit(int pnum)
 
 
 //----------------------------------------------------------------------------|
-// Global Functions                                                           |
+// Extern Functions                                                           |
 //
-
-//
-// DUD_AddWeapon
-//
-void DUD_AddWeapon(int slot, int weap)
-{
-   DUD_Weapon[slot][weap].used = true;
-}
 
 //
 // DUD_DoAmmoCap
--- src/DUDamned/code/dud_weap.h
+++ src/DUDamned/code/dud_weap.h
@@ -6,14 +6,12 @@
 //
 //-----------------------------------------------------------------------------
 //
-// Ability handling.
+// Weapon and ammo handling.
 //
 //-----------------------------------------------------------------------------
 
-#ifndef DUD_ABIL_H__
-#define DUD_ABIL_H__
-
-#include "dud_weap.h"
+#ifndef DUD_WEAP_H__
+#define DUD_WEAP_H__
 
 #include "du_defs.h"
 
@@ -22,48 +20,68 @@
 // Types                                                                      |
 //
 
-typedef struct dud_ability_t dud_ability_t;
+typedef struct dud_ammo_t   dud_ammo_t;
+typedef struct dud_weapon_t dud_weapon_t;
 
 enum
 {
-   ABIL1_AMMO = 0,
-   ABIL1_DAMAGE_CLIP,
-   ABIL1_DAMAGE_SHEL,
-   ABIL1_DAMAGE_RCKT,
-   ABIL1_DAMAGE_CELL,
-   ABIL1_BERSERK,
+   AMMO_CLIP,
+   AMMO_SHEL,
+   AMMO_RCKT,
+   AMMO_CELL,
 
-   ABIL2_AMMO = 0,
-   ABIL2_ARMOR,
-   ABIL2_RADIUS,
-   ABIL2_PSPEED,
+   AMMOMAX
+};
 
-   ABIL3_AMMO = 0,
-   ABIL3_HEALTH,
-   ABIL3_DAMAGE,
-   ABIL3_SPEED,
-   ABIL3_JUMP,
+enum
+{
+   WEAP1_FIST = 0,
+   WEAP1_CSAW,
+   WEAP1_CFIST,
 
-   ABILMAX = 7
+   WEAP2_PIST = 0,
+
+   WEAP3_SHTG = 0,
+   WEAP3_SHT2,
+
+   WEAP4_CGUN = 0,
+   WEAP4_DGUN,
+
+   WEAP5_RCKT = 0,
+
+   WEAP6_PLAS = 0,
+   WEAP6_LASG,
+
+   WEAP7_BFG9 = 0,
+   WEAP7_RAIL,
+
+   WEAPMAX = 3
 };
 
 //
-// dud_ability_t
+// dud_ammo_t
 //
-struct dud_ability_t
+struct dud_ammo_t
+{
+   int   ammo;
+   int   cap;
+   int   regen;
+   __str name;
+   __str nameDec;
+};
+
+//
+// dud_weapon_t
+//
+struct dud_weapon_t
 {
    bool  used;
-   accum ammoCap[AMMOMAX];
-   int   ammoRegen[AMMOMAX];
-   accum damage[AMMOMAX];
-   accum radius[AMMOMAX];
-   accum pspeed[AMMOMAX];
-   int   armorRegen, healthRegen;
-   accum speed, jumpz;
+   int   ammo;
    __str desc;
    __str icon;
    __str iconDim;
    __str name;
+   __str nameDec;
    __str nameIco;
 };
 
@@ -72,17 +90,17 @@ struct dud_ability_t
 // Extern Objects                                                             |
 //
 
-extern dud_ability_t  DUD_Ability[3][ABILMAX];
-extern dud_ability_t *DUD_AbilitySelected[MAX_PLAYERS][3];
+extern dud_ammo_t DUD_Ammo[AMMOMAX];
+
+extern dud_weapon_t  DUD_Weapon[7][WEAPMAX];
+extern dud_weapon_t *DUD_WeaponSelected[MAX_PLAYERS][7];
 
 
 //----------------------------------------------------------------------------|
 // Extern Functions                                                           |
 //
 
-accum DUD_GetDamageFactor(int pnum, int ammo);
-accum DUD_GetPSpeedFactor(int pnum, int ammo);
-accum DUD_GetRadiusFactor(int pnum, int ammo);
+void DUD_AddWeapon(int slot, int weap);
 
-#endif//DUD_ABIL_H__
+#endif//DUD_WEAP_H__
 
